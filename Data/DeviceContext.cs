a422d5324fa962e0e985db03fad715e6f3f37fe2
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-05-05 16:50:03 +0200

    Implement DeviceLogic

--- Data/DeviceContext.cs
+++ Data/DeviceContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 
@@ -22,13 +23,60 @@ namespace Data
             {
                 optionsBuilder.
                     UseLazyLoadingProxies().
-                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\DeviceDB.mdf;integrated security=True;MultipleActiveResultSets=True");
+                    UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=DeviceDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new { Id = "341743f0-asd2–42de-afbf-59kmkkmk72cf6", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "341743f0-dee2–42de-bbbb-59kmkkmk72cf6", Name = "Customer", NormalizedName = "CUSTOMER" }
+            );
+
+            var appUser = new IdentityUser
+            {
+                Id = "02174cf0–9412–4cfe-afbf-59f706d72cf6",
+                Email = "kemenes.janos@stud.uni-obuda.hu",
+                NormalizedEmail = "kemenes.janos@stud.uni-obuda.hu",
+                EmailConfirmed = true,
+                UserName = "kemenes.janos@stud.uni-obuda.hu",
+                NormalizedUserName = "kemenes.janos@stud.uni-obuda.hu",
+                SecurityStamp = string.Empty
+            };
+
+            var appUser2 = new IdentityUser
+            {
+                Id = "e2174cf0–9412–4cfe-afbf-59f706d72cf6",
+                Email = "kovacs.andras@nik.uni-obuda.hu",
+                NormalizedEmail = "kovacs.andras@nik.uni-obuda.hu",
+                EmailConfirmed = true,
+                UserName = "kovacs.andras@nik.uni-obuda.hu",
+                NormalizedUserName = "kovacs.andras@nik.uni-obuda.hu",
+                SecurityStamp = string.Empty
+            };
+
+            appUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "titok");
+            appUser2.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "jelszo");
+
+
+            modelBuilder.Entity<IdentityUser>().HasData(appUser);
+            modelBuilder.Entity<IdentityUser>().HasData(appUser2);
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = "341743f0-asd2–42de-afbf-59kmkkmk72cf6",
+                UserId = "02174cf0–9412–4cfe-afbf-59f706d72cf6"
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = "341743f0-dee2–42de-bbbb-59kmkkmk72cf6",
+                UserId = "e2174cf0–9412–4cfe-afbf-59f706d72cf6"
+            });
+
 
             modelBuilder.Entity<Method>(entity =>
             {
---
+++ Logic/DeviceLogic.cs
@@ -0,0 +1,91 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    public class DeviceLogic
+    {
+        IRepository<Device> DeviceRepo;
+
+        public DeviceLogic(IRepository<Device> DeviceRepo)
+        {
+            this.DeviceRepo = DeviceRepo;
+        }
+
+        //crud methods
+
+        public void AddDevice(Device item)
+        {
+            this.DeviceRepo.Add(item);
+        }
+
+        public void DeleteDevice(string uid)
+        {
+            this.DeviceRepo.Delete(uid);
+        }
+
+        public IQueryable<Device> GetAllDevice()
+        {
+            return DeviceRepo.Read();
+        }
+
+        public Device GetDevice(string uid)
+        {
+            return DeviceRepo.Read(uid);
+        }
+
+        public void UpdateDevice(string uid, Device newitem)
+        {
+            DeviceRepo.Update(uid, newitem);
+        }
+
+        //non-crud methods/properties
+
+        //public double AVGVideosCount
+        //{
+        //    get
+        //    {
+        //        return GetAllDevice().Average(t => t.Videos.Count());
+        //    }
+        //}
+
+        //public void AddVideoToDevice(Video video, string DeviceId)
+        //{
+        //    GetDevice(DeviceId).Videos.Add(video);
+        //    DeviceRepo.Save();
+        //}
+        //public void RemoveVideoFromDevice(Video video, string DeviceId)
+        //{
+        //    GetDevice(DeviceId).Videos.Remove(video);
+        //    videoRepo.Delete(video.Uid);
+        //    DeviceRepo.Save();
+        //}
+
+        public void FillDbWithSamples()
+        {
+            Method m1 = new Method() { Description = "m1 des", Name = "m1" };
+            Method m2 = new Method() { Description = "m2 des", Name = "m2" };
+            Method m3 = new Method() { Description = "m3 des", Name = "m3" };
+
+            Property p1 = new Property() { Description = "d1 des", Name = "d1" };
+            Property p2 = new Property() { Description = "d2 des", Name = "d2" };
+
+            Device d1 = new Device() { Type = deviceType.I2C, DescriptionTabData = new DescriptionTabData() {Description="Display.", ShortDescription="Disp", Name="Display", Example="alma"} };
+            Device d2 = new Device() { Type = deviceType.Switch, DescriptionTabData = new DescriptionTabData() { Description = "Switch.", ShortDescription = "switch", Name = "Switch", Example = "körte" } };
+
+            GetDevice(d1.Id).Methods.Add(m1);
+            GetDevice(d1.Id).Methods.Add(m2);
+            GetDevice(d2.Id).Methods.Add(m3);
+            GetDevice(d2.Id).Properties.Add(p1);
+            GetDevice(d2.Id).Properties.Add(p2);
+
+            DeviceRepo.Save();
+
+        }
+
+    }
+}
